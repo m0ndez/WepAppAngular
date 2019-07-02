1a740dcdd905d071efe7c5e2b87a1740f0eb5a40
m0nd3z <chattongchai.r@gmail.com>
2019-07-02 16:33:30 +0700

    Update

--- src/app/users/view/view.component.ts
+++ src/app/users/view/view.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Directive, OnInit} from '@angular/core';
 import {MDBModalRef} from 'angular-bootstrap-md';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss']
 })
+
 export class ViewComponent implements OnInit {
+  // viewForm: FormGroup
 
-  constructor(public modalRef: MDBModalRef) { }
+  viewForm = this.fb.group( {
+    firstname: [Validators.required],
+    lastname: [Validators.required],
+    phonenumber: [Validators.required],
+    email: [Validators.required]
+  })
+  isDisable = true;
 
-  ngOnInit() {
-    this.data()
+  constructor(public modalRef: MDBModalRef, private fb: FormBuilder) { }
+
+  ngOnInit() {/*
+    this.viewForm = new FormGroup({
+      optionEdit: new FormControl({disabled: true, value: ''},[Validators.required]),
+      firstname: new FormControl({disabled: true, value: ''},[Validators.required]),
+      lastname: new FormControl({disabled: true, value: ''},[Validators.required]),
+      phonenumber: new FormControl({disabled: true, value: ''}, [Validators.required]),
+      email: new FormControl({disabled: true, value: ''}, [Validators.required])
+    });*/
   }
-  data() {
-    console.log(this.modalRef)
+  /*
+  get optionEdit() {
+    return this.viewForm.get('optionEdit') as FormControl
+  }
+   */
+  toggleEdit() {
+    this.isDisable = !this.isDisable
   }
 
+  isEdit() {
+    console.log(this.viewForm.value)
+  }
 }
